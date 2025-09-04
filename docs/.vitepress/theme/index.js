3c285cc49a1f8e55e11c0d6801bd5e0ef25e68ea
Wxh1104 <wxh8953717@gmail.com>
2025-09-04 23:24:20 +0800

    更改Iconify API请求 2

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,27 +1,19 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
-
-// 1. 从 Iconify 核心库中导入 'Iconify' 对象
-import { Iconify } from '@iconify/vue'
-
-// 2. 导入我们之前创建的“增强版”Icon组件的定义逻辑
-import { Icon as OriginalIcon } from '@iconify/vue'
-
+import { Icon as OriginalIcon, addAPIProvider } from '@iconify/vue'
 import './custom.css'
 
 // --- 关键修复：强制 Iconify 使用 HTTPS API ---
-// 这会覆盖默认的 API 提供商，确保所有请求都是安全的
-Iconify.addAPIProvider('iconify', {
+// 在最新版本中，addAPIProvider 直接从 @iconify/vue 导出，非常方便
+addAPIProvider('iconify', {
   resources: ['https://api.iconify.design'],
 });
 // --- 修复结束 ---
-
 
 // 我们之前创建的增强版 Icon 组件定义 (保持不变)
 const EnhancedIcon = {
   props: ['icon', 'href', 'color'],
   setup(props, { attrs }) {
-    // ... (这里是您之前的组件逻辑，无需修改)
     return () => {
       const iconAttrs = { ...attrs, icon: props.icon };
       if (props.color === 'brand') {
