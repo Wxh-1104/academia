a0d0cfe9b3fc3178b459db17c1a587c22a6cd58e
Wxh1104 <wxh8953717@gmail.com>
2025-09-05 22:22:39 +0800

    取消强制emoji

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,10 +2,6 @@ import DefaultTheme from "vitepress/theme";
 import { h } from "vue";
 import { Icon as OriginalIcon, addAPIProvider } from "@iconify/vue";
 
-import { onMounted, watch, nextTick } from 'vue'
-import { useRoute } from 'vitepress'
-import twemoji from 'twemoji'
-
 import "./custom.css";
 
 // --- 关键修复：强制 Iconify 使用 HTTPS API ---
@@ -48,44 +44,5 @@ export default {
 
   enhanceApp({ app }) {
     app.component("Icon", EnhancedIcon);
-  },
-
-  setup() {
-    // 获取 VitePress 的路由对象，以便我们能监听其变化
-    const route = useRoute();
-
-    // 定义一个可复用的函数，用于解析和替换 Emoji
-    const parseEmojis = () => {
-      // nextTick 确保我们在 Vue 完成 DOM 更新之后再执行操作
-      // 这可以防止我们尝试操作还不存在的元素
-      nextTick(() => {
-        // twemoji.parse 会扫描指定的 DOM 元素
-        // 并将找到的 Unicode Emoji 字符替换为 <img> 标签
-        const content = document.querySelector(".vp-doc");
-        if (content) {
-          twemoji.parse(content, {
-            // 使用 SVG 格式，它们更清晰，并且可以无限缩放
-            folder: "svg",
-            ext: ".svg",
-            // 使用公共 CDN 来提供 SVG 图片，性能好且无需自己托管
-            base: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/",
-          });
-        }
-      });
-    };
-
-    // onMounted() 会在组件首次挂载到 DOM 后执行
-    // 我们用它来处理页面的初次加载
-    onMounted(() => {
-      parseEmojis();
-    });
-
-    // watch() 会监听一个响应式数据的变化
-    // 这是处理 SPA 导航的关键！
-    // 当 route.path (即页面 URL) 改变时，我们就重新运行解析函数
-    watch(
-      () => route.path,
-      () => parseEmojis()
-    );
   },
 };
